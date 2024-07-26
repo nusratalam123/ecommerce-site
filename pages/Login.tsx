2a20fc4247cbfa245ac9861ef0bddff2078cfba1
nusratalam <nusratalam975@gmail.com>
2024-07-26 23:35:09 +0600

    add login page

---
+++ pages/Login.tsx
@@ -0,0 +1,71 @@
+import Link from "next/link";
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa6";
+import styles from "../styles/login.module.css";
+import { useEffect, useState } from "react";
+
+export default function Login() {
+  [showPassord, setShowPassword] = useState(false);
+  return (
+    <>
+      <section id="login" className={styles.login}>
+        <div className={styles.container}>
+          <div className={styles.card}>
+            <div className={styles.cardHeader}>
+              <h1 className={styles.title}>
+                Welcome to our website! Please Login
+              </h1>
+            </div>
+
+            <form className={styles.form}>
+              <div className="grid">
+                <label className={styles.label}>Email:</label>
+                <div className={styles.inputContainer}>
+                  <input
+                    type="email"
+                    placeholder="enter email"
+                    name="email"
+                    className={styles.input}
+                  />
+                </div>
+              </div>
+
+              <div>
+                <label className={styles.label}>Password:</label>
+                <div
+                  className={`${styles.inputContainer} ${styles.passwordContainer}`}
+                >
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    placeholder="enter password"
+                    // value={data.password}
+                    name="password"
+                    // onChange={handleOnChange}
+                    className={styles.input}
+                  />
+                  <div
+                    className={styles.eyeIcon}
+                    // onClick={() => setShowPassword((prev) => !prev)}
+                  >
+                    <span>{showPassword ? <FaEyeSlash /> : <FaEye />}</span>
+                  </div>
+                </div>
+                <Link href="/forgot-password" className={styles.forgotPassword}>Forgot password?
+                </Link>
+              </div>
+
+              <button className={styles.button}>Login</button>
+            </form>
+
+            <p className="my-5">
+              {/* Don't have account?{" "} */}
+              <Link href="/signup"
+                 className={styles.signUpLink}>Sign up
+              </Link>
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,21 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Header from "@/components/Header";
+import Featured from "@/components/Featured";
+import Footer from "@/components/Footer";
 
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-   <Header></Header>
+    <>
+      <Header></Header>
+      <Featured />
+
+      <Footer></Footer>
+    </>
   );
 }
+
+
